44415d0541ad941f9a2120570b82f2a6873fd9c8
AnaLia506 <84133773+AnaLia506@users.noreply.github.com>
2021-07-23 17:45:12 -0300

    Create nafta.js

---
+++ nafta.js
@@ -0,0 +1,11 @@
+const Rendimiento = document.querySelector("#rendimiento");
+const Recorrido = document.querySelector("#kilometros");
+const NaftaConsumida;
+NaftaConsumida = Recorrido / Rendimiento;
+const CostoViaje;
+CostoViaje = NaftaConsumida;
+
+const Super95 = 65.81;
+const Premium = 67.74;
+const S10 = 67.87;
+const S50 = 45.7;
